bc65d70ad7596ba3469e329007dce7d30e9540d2
Zoe Johnston <82781352+ZoeJ2020@users.noreply.github.com>
2022-05-25 12:46:16 +0100

    fixed logo CSS and userValidate function

--- script/index.js
+++ script/index.js
@@ -1,33 +1,66 @@
 function userValidate()
 {
-  document.getElementById('emailError').innerText = "";
-  document.getElementById('userError').innerText = "";
+  let emailErr =  document.getElementById('emailError');
+  let userErr =  document.getElementById('userError');
+
+  emailErr.innerText="";
+  userErr.innerText="";
 
   let userEmail = document.getElementById('userEmail').value;
   let validEmail = userEmail.includes('@');
   let userName = document.getElementById('userName').value;
 
-
-  if(userEmail) // if userEmail has been filled in, then
+  if(userEmail && userName) //if userEmail and userName inputs are filled
   {
-    if(validEmail != false) //if email is valid then
-    {
-      if(userName) //if username exists/has a value then
-      {
-        checkCookie('userName');
-        loadUser();
-      } else //if username is empty
-      {
-        document.getElementById('userError').innerText = "Error: Username field empty.";
-      }
 
-    } else{
-      document.getElementById('emailError').innerText = "Error: Invalid email entered.";
+    if(validEmail == true)
+    {
+      loadUser();
+    } else
+    {
+      emailErr.innerText="Email is not valid";
     }
 
-  } else { //if userEmail is null or empty, display error
-    document.getElementById('emailError').innerText = "Error: email field is empty";
+  } else 
+
+  {
+    if(!userEmail) //if userEmail is missing, display message
+    {
+      emailErr.innerText="Email is missing";
+    }  else
+    {
+      if(!validEmail)
+      {
+        emailErr.innerText="Email is not valid";
+      }
+    }
+    
+    if(!userName) //if userName is missing, display message
+    {
+      userErr.innerText="User is missing";
+    }
   }
+
+  // if(userEmail) // if userEmail has been filled in, then
+  // {
+  //   if(validEmail != false) //if email is valid then
+  //   {
+  //     if(userName) //if username exists/has a value then
+  //     {
+  //       checkCookie('userName');
+  //       loadUser();
+  //     } else //if username is empty
+  //     {
+  //       document.getElementById('userError').innerText = "Error: Username field empty.";
+  //     }
+
+  //   } else{
+  //     document.getElementById('emailError').innerText = "Error: Invalid email entered.";
+  //   }
+
+  // } else { //if userEmail is null or empty, display error
+  //   document.getElementById('emailError').innerText = "Error: email field is empty";
+  // }
 }
 
 function loadUser()
@@ -35,11 +68,4 @@ function loadUser()
   window.location.replace("/menu.html");
 }
 
-  //FUNCTION LIST
-
-  // If email includes @, proceed
-  //if not, display error
-  
-  //proceed
-  //check username input value, if saved in username cookie then go to menu page
-  //if username does not exist then create new cookie
+// if 
